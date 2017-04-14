be9e1a339d039994bc7d65b4e4d07746656fa128
Alex <he.fa@husky.neu.edu>
2017-04-13 23:58:13 -0400

    separate big php block

--- Final/friend_page-action.php
+++ Final/friend_page-action.php
@@ -9,7 +9,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   if(!empty($_POST['comment']) ){
      $message = $_POST['comment'];
       $id= $_POST['postId'];
-      $sql_insert = "insert into Comments (Content,User_Id,Post_Id) values ('$message',2,'$id');";
+      $sql_insert = "insert into Comments (Content,User_Id,Post_Id) values ('$message','$currentId','$id');";
        //ob_start();
       $result = mysqli_query($conn, $sql_insert);
       if($result==false){
