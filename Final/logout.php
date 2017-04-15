9f73512fd96568c322453a039dfca4adab6c15c3
FeifeifeiGit <lifei0160@gmail.com>
2017-04-15 12:49:47 -0400

    offline done!
    offline done! and userHome adjust

--- Final/logout.php
+++ Final/logout.php
@@ -1,12 +1,16 @@
 <?php
 session_start();
 // remove all session variables
+ $currentId =$_SESSION['userId'];
+include "db.php";
+$offlineQuery ="UPDATE Users SET Online=0 WHERE User_Id = '$currentId'";
+$offlineResult=mysqli_query($conn, $offlineQuery);
+$row = mysqli_fetch_assoc($offlineResult);
+
 session_unset(); 
 
 // destroy the session 
 session_destroy(); 
-//echo var_dump($_SESSION);
-//echo var_dump($_SESSION['userId']);
 header('Location: login.php');
-//echo "isset".isset($_SESSION['userId']);
+
 ?>
