5200f55e8f5b7a07af836ae65fd00dc5e059ff7b
Alex <he.fa@husky.neu.edu>
2017-04-16 21:57:50 -0400

    fix header in add and delete request

--- Final/friend_page-action.php
+++ Final/friend_page-action.php
@@ -89,12 +89,14 @@ if(!empty($_GET['agreeAdd'])){
   //add friend in two ways
   $addFriendResult=mysqli_query($conn, "INSERT INTO FriendsList (User_Id, Friend_Id) VALUES ('$currentId', '$friendId')");
   $addFriendRvsResult=mysqli_query($conn, "INSERT INTO FriendsList (User_Id, Friend_Id) VALUES ('$friendId', '$currentId')");
+  header("location: userPage.php?userId=$currentId");
 }
 
 //user does not accept request and delete the request
 if(!empty($_GET['deleteRqst'])){
   $friendId=$_GET['deleteRqst'];
   $deleteResult=mysqli_query($conn, "DELETE FROM Request WHERE Sender_Id='$friendId' AND Receiver_Id='$currentId';");
+  header("location: userPage.php?userId=$currentId");
 }
 
 
